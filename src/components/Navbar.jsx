1185429b761cd61f3cb43beb1bb03746436829b2
priyanshrm <priyanshmahendra@gmail.com>
2023-02-22 15:35:40 +0530

    Add Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar(){
+    return (
+        <div className="top--section">
+            <nav className="navbar">
+                <img src='./globe.svg' className="navbar--img"></img>
+                <p className="navbar--text">my travel journal.</p>            
+            </nav>
+        </div>
+    )
+}
